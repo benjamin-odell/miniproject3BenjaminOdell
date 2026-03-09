363e041b64fd288d3c682150904b123ad8232c38
Benjamin Odell <benjamin.m.odell@proton.me>
2026-03-09 17:54:06 -0500

    Rename blog module to frogbook and update blueprint registration

--- FrogBook/__init__.py
+++ FrogBook/__init__.py
@@ -29,8 +29,8 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
-    from . import blog
-    app.register_blueprint(blog.bp)
+    from . import frogbook
+    app.register_blueprint(frogbook.bp)
     app.add_url_rule('/', endpoint='index')
 
     return app
