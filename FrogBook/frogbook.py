264a25c94aa54bb13e7eff8ddde78747c0dbcf04
Benjamin Odell <benjamin.m.odell@proton.me>
2026-03-12 16:17:21 -0500

    Enhance UI with Bootstrap integration for improved layout and styling across multiple pages

--- FrogBook/frogbook.py
+++ FrogBook/frogbook.py
@@ -23,8 +23,8 @@ def index():
     frogs = db.execute(
         'SELECT *'
         ' FROM frog f JOIN user u ON f.user_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
+        ' ORDER BY elo DESC'
+    ).fetchmany(size=10)
     return render_template('frogbook/index.html', frogs=frogs)
 
 @bp.route('/my_frogs')
@@ -188,51 +188,6 @@ def create():
             return redirect(url_for('frogbook.index'))
 
     return render_template('frogbook/create.html')
-
-
-def get_post(id, check_author=True):
-    post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
-    ).fetchone()
-
-    if post is None:
-        abort(404, f"Post id {id} doesn't exist.")
-
-    if check_author and post['author_id'] != g.user['id']:
-        abort(403)
-
-    return post
-
-
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
-@login_required
-def update(id):
-    post = get_post(id)
-
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
-
-        if not title:
-            error = 'Title is required.'
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE post SET title = ?, body = ?'
-                ' WHERE id = ?',
-                (title, body, id)
-            )
-            db.commit()
-            return redirect(url_for('frogbook.index'))
-
-    return render_template('frogbook/update.html', post=post)
 
 
 @bp.route('/<int:id>/delete', methods=('POST',))
